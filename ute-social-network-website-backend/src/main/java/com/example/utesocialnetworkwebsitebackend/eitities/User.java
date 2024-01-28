957c822a03d1982fb35b035b04827a35c41af9b2
Tân <leetaan1902@gmail.com>
2024-01-28 10:59:44 +0700

    base

--- ute-social-network-website-backend/src/main/java/com/example/utesocialnetworkwebsitebackend/eitities/User.java
+++ ute-social-network-website-backend/src/main/java/com/example/utesocialnetworkwebsitebackend/eitities/User.java
@@ -1,2 +1,55 @@
-package com.example.utesocialnetworkwebsitebackend.eitities;public class User {
+package com.example.utesocialnetworkwebsitebackend.eitities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+@Table(name = "Customers")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "customer_id")
+    private Long customerId;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    private String email;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "account_balance")
+    private int accountBalance;
+
+    @Column(name="password_hash")
+    private String passHash;
+
+    @Enumerated(EnumType.STRING)
+    private ERole role;
+
+    @OneToMany(mappedBy = "customer",fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    List<Transaction> transactions;
+
+    @OneToMany(mappedBy = "customer",fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    List<Orders> orders;
+
+    @OneToMany(mappedBy = "customer",fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    List<Ticket> tickets;
+
+
+
 }
+
